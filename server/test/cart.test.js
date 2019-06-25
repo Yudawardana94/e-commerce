890f2cde3b5c091239d3852839dea09b262e7e46
yudawardana <yuda.wardana40@gmail.com>
2019-06-25 11:17:22 +0700

    init vue cli, modif cart.test

--- server/test/cart.test.js
+++ server/test/cart.test.js
@@ -4,4 +4,39 @@ const chaiHttp = require('chai-http');
 const app = require('../app');
 
 chai.use(chaiHttp);
-let express = chai.expect;
+let expect = chai.expect;
+
+describe('Cart CRUD',function () {
+    describe ('GET /cart/findAll',function(){
+        describe('Success',function() {
+            
+        }),
+        describe('Failed / Error', function() {
+
+        })
+    }),
+    describe('POST /cart/add', function(){
+        describe('Success',function() {
+
+        }),
+        describe('Failed / Error', function() {
+            
+        })
+    }),
+    describe('PATCH /cart/update', function() {
+        describe('Success',function() {
+
+        }),
+        describe('Failed / Error', function() {
+            
+        })
+    }),
+    describe('DELETE /cart/delete', function(){
+        describe('Success',function() {
+
+        }),
+        describe('Failed / Error', function() {
+            
+        })
+    })
+})
